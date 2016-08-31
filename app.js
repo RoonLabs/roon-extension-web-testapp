4e40cf28d19acc6c81e07aabf57349e7c3e307c3
Danny Dulai <danny@ishiboo.com>
2016-08-31 16:36:59 -0400

    browserstarted

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var RoonApi          = require("node-roon-api"),
     RoonApiTransport = require('node-roon-api-transport'),
     RoonApiImage     = require('node-roon-api-image'),
+    RoonApiBrowse    = require('node-roon-api-browse'),
     Vue              = require('vue');
 
 var core;
@@ -45,13 +46,21 @@ var v = new Vue({
     el: "#app",
     template: require('./root.html'),
     data: function() { return {
-	status: 'foo',
-        zones: [],
+	status:          'foo',
+        zones:           [],
         current_zone_id: null,
+        listoffset:      0,
+        list:            null,
+        items:           [],
     }},
     computed: {
         zone: function () {
             return this.zones[this.current_zone_id];
+        }
+    },
+    watch: {
+        'current_zone_id': function(val, oldval) {
+            refresh_browse();
         }
     },
     methods: {
@@ -67,8 +76,81 @@ var v = new Vue({
         transport_next: function() {
             core.services.RoonApiTransport.control(this.zone, 'next');
         },
+        list_item: function(item) {
+            refresh_browse({ item_key: item.item_key });
+        },
+        list_back: function() {
+            refresh_browse({ pop_levels: 1 });
+        },
+        list_home: function() {
+            refresh_browse({ pop_all: true });
+        },
+        list_refresh: function() {
+            refresh_browse({ refresh_list: true } );
+        },
+        list_next_page: function() {
+            load_browse(this.listoffset + 100);
+        },
+        list_prev_page: function() {
+            load_browse(this.listoffset - 100);
+        },
     }
 });
+
+function refresh_browse(opts) {
+    opts = Object.assign({
+        hierarchy:          "browse",
+        zone_or_output_id:  v.current_zone_id,
+        set_display_offset: v.listoffset,
+    }, opts);
+
+    core.services.RoonApiBrowse.browse(opts, (err, r) => {
+        if (err) { console.log(err, r); return; }
+
+        console.log(err, r);
+
+        if (r.action == 'list') {
+            v.$set("list", r.list);
+            v.$set("items", []);
+            var listoffset = r.list.display_offset > 0 ? r.list.display_offset : 0;
+            load_browse(listoffset);
+
+        } else if (r.action == 'message') {
+            alert((r.is_error ? "ERROR: " : "") + r.message);
+
+        } else if (r.action == 'replace_item') {
+            var i = 0;
+            var l = v.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1, r.item);
+                    break;
+                }
+                i++;
+            }
+            v.$set("items", l);
+
+        } else if (r.action == 'remove_item') {
+            var i = 0;
+            var l = v.items;
+            while (i < l.length) {
+                if (l[i].item_key == opts.item_key) {
+                    l.splice(i, 1);
+                    break;
+                }
+                i++;
+            }
+            v.$set("items", l);
+        }
+    });
+}
+
+function load_browse(listoffset) {
+    core.services.RoonApiBrowse.load({ hierarchy: "browse", offset: listoffset, set_display_offset: listoffset }, (err, r) => {
+        v.$set("listoffset", listoffset);
+        v.$set("items", r.items);
+    });
+}
 
 var extension = roon.extension({
     extension_id:        'com.roonlabs.web.testapp',
@@ -76,7 +158,7 @@ var extension = roon.extension({
     display_version:     "1.0.0",
     publisher:           'Roon Labs, LLC',
     email:               'contact@roonlabs.com',
-    required_services:   [ RoonApiTransport, RoonApiImage ],
+    required_services:   [ RoonApiBrowse, RoonApiTransport, RoonApiImage ],
     optional_services:   [ ],
     provided_services:   [ ],
 
@@ -95,6 +177,9 @@ var extension = roon.extension({
             }
         });
         v.status = 'connected';
+
+        v.listoffset = 0;
+        refresh_browse();
     },
     core_unpaired: function(core_) {
 	core = undefined;
