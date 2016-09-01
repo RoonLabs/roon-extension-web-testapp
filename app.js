bf8441215245b2e01b1b2148cfd770ad31fdf4a2
Danny Dulai <danny@ishiboo.com>
2016-09-01 11:54:52 -0400

    added seek

--- app.js
+++ app.js
@@ -34,6 +34,17 @@ var v = new Vue({
         }
     },
     methods: {
+        to_time: function (s) {
+            let r = "";
+            if (s >= 3600) {           r += Math.floor(s / 3600).toString() + ":"; s = s % 3600; }
+
+            if      (!r || s >= 600)  {           r += Math.floor(s / 60).toString()   + ":"; s = s % 60;   }
+            else                { r += "0"; r += Math.floor(s / 60).toString()   + ":"; s = s % 60;   }
+
+            if      (s >= 10)   {           r +=                  s.toString();         s = -1;       }
+            else if (s >= 0)    { r += "0"; r +=                  s.toString();         s = -1;       }
+            return r;
+        },
         transport_playpause: function() {
             core.services.RoonApiTransport.control(this.zone, 'playpause');
         },
